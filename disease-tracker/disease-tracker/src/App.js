23827fafb381158a4795fca985956c95ad4de96f
Amogh Maroju <amogh.gyana@gmail.com>
2024-08-19 20:08:38 -0500

    map countries to dropdown test

--- disease-tracker/disease-tracker/src/App.js
+++ disease-tracker/disease-tracker/src/App.js
@@ -1,19 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import { MenuItem, Select, FormControl } from "@mui/material";
 
 const App = () => {
+  const [countries, setCountries] = useState(["USA", "Canada", "Mexico"]);
+
   return (
     <div className="App">
-      <h1>Disease Tracker</h1>
+      <div className="app_header">
+        <h1>Disease Tracker</h1>
 
-      <FormControl className="app_dropdown">
-        <Select variant="outlined" value="abc">
-          <MenuItem value="worldwide">worldwide</MenuItem>
-          <MenuItem value="worldwide">worldwide</MenuItem>
-          <MenuItem value="worldfdsfsdwide">worldwfdfsfdide</MenuItem>
-        </Select>
-      </FormControl>
+        <FormControl className="app_dropdown">
+          <Select variant="outlined" value="abc">
+            {/* Loop through countries and show a drop down */}
+            {countries.map((country) => (
+              <MenuItem value={country}>{country}</MenuItem>
+            ))}
+
+            {/* <MenuItem value="worldwide">worldwide</MenuItem>
+            <MenuItem value="worldwide">worldwide</MenuItem>
+            <MenuItem value="worldfdsfsdwide">worldwfdfsfdide</MenuItem> */}
+          </Select>
+        </FormControl>
+      </div>
     </div>
   );
 };
