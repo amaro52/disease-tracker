864a85d33539c8c5649b789edffa9cbe780f171a
Amogh Maroju <amogh.gyana@gmail.com>
2024-08-21 16:01:08 -0500

    Added dropdown country selected

--- disease-tracker/disease-tracker/src/App.js
+++ disease-tracker/disease-tracker/src/App.js
@@ -1,27 +1,57 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
 import { MenuItem, Select, FormControl } from "@mui/material";
+import axios from "axios";
 
 const App = () => {
-  const [countries, setCountries] = useState(["USA", "Canada", "Mexico"]);
+  const [countries, setCountries] = useState([]);
+  const [country, setCountry] = useState("worldwide");
+
+  useEffect(() => {
+    const getCountriesData = async () => {
+      const response = await axios.get(
+        "https://disease.sh/v3/covid-19/countries"
+      );
+      const result = response.data;
+
+      const countries = result.map((country) => ({
+        name: country.country,
+        value: country.countryInfo.iso2,
+      }));
+
+      setCountries(countries);
+    };
+
+    getCountriesData();
+  }, []);
+
+  // when selected country is changed in the drop down, change the display name on the menu
+  const onCountryChange = (event) => {
+    const countryCode = event.target.value;
+    setCountry(countryCode);
+
+    console.log(countryCode);
+  };
 
   return (
     <div className="App">
       <div className="app_header">
+        {/* Title */}
         <h1>Disease Tracker</h1>
 
+        {/* Header */}
         <FormControl className="app_dropdown">
-          <Select variant="outlined" value="abc">
-            {/* Loop through countries and show a drop down */}
+          <Select variant="outlined" value={country} onChange={onCountryChange}>
+            {/* Map countires to the drop down */}
+            <MenuItem value="worldwide">Worldwide</MenuItem>
             {countries.map((country) => (
-              <MenuItem value={country}>{country}</MenuItem>
+              <MenuItem value={country.value}>{country.name}</MenuItem>
             ))}
-
-            {/* <MenuItem value="worldwide">worldwide</MenuItem>
-            <MenuItem value="worldwide">worldwide</MenuItem>
-            <MenuItem value="worldfdsfsdwide">worldwfdfsfdide</MenuItem> */}
           </Select>
         </FormControl>
+
+        {/* Infoboxes */}
+        
       </div>
     </div>
   );
